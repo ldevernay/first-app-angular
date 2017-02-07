2dabc6f8e13a0d51f28d10fa5b7eaa04dc745187
ldevernay <boblehamster@yahoo.fr>
2017-02-07 14:04:35 +0100

    first data-binding

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,6 @@
  * Controller of the firstAppApp
  */
 angular.module('firstAppApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope) {
+    $scope.helloWorld = 'Hello World!';
   });
